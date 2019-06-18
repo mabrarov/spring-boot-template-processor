88c2da9bd85127125e736a8b9bb4fb402cbf7469
Marat Abrarov <abrarov@gmail.com>
2019-06-18 20:45:47 +0300

    Initial wireframe of Spring Boot application

---
+++ README.md
@@ -0,0 +1,4 @@
+Spring Boot Template Processor
+============
+
+Template processor utilizing Spring Boot relaxed binding for data source
---
+++ src/main/java/org/mabrarov/springboottemplateprocessor/Main.java
@@ -0,0 +1,18 @@
+package org.mabrarov.springboottemplateprocessor;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
+
+  @Override
+  public void run(String... args) {
+    System.out.println("Hello world!");
+  }
+}
